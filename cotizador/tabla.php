ac966d89d77131aaab445e1d88410f4f6fc8d646
educamo <educamo@gmail.com>
2020-05-31 05:24:19 -0400

    se corrigio la conexcion a la base de datos en el archibo tabla, ya que apuntaba  a otro directorio

--- cotizador/tabla.php
+++ cotizador/tabla.php
@@ -6,8 +6,8 @@
     ---------------------------*/
 
     /* Connect To Database*/
-    require_once ("/config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-    require_once ("/config/conexion.php");//Contiene funcion que conecta a la base de datos
+    require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
+    require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
 
     $sql = "SELECT id_cotizacion, 
     numero_cotizacion, 
