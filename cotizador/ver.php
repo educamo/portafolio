4395335b4e3107bdb97278c4a82d134a811e6119
educamo <educamo@gmail.com>
2020-07-17 16:12:30 -0400

    se corrigio que se mostrara los productos de la cotizacion en el modal de ver contizacion desde el panel admin

--- cotizador/ver.php
+++ cotizador/ver.php
@@ -1,8 +1,8 @@
 <?php 
-$prueba = $_GET["dat"]; 
+$prueba = $_GET['dat']; 
 
-require_once ("/config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
-    require_once ("/config/conexion.php");//Contiene funcion que conecta a la base de datos
+require_once ("config/db.php");//Contiene las variables de configuracion para conectar a la base de datos
+    require_once ("config/conexion.php");//Contiene funcion que conecta a la base de datos
 
     $sql = "SELECT
     cot.atencion,
